390a14a4254ffe7d0f971dd6367fb6242999301e
chris2go <weiyq_chris@126.com>
2018-04-07 21:01:10 +0800

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -676,60 +676,7 @@ var mydefault11 = [{
                     }
           }]
 
-var test123 =[{
-		"featureType":"water",
-		"elementType":"geometry",
-		"stylers":{
-			"color":"#e9e9e9","lightness":17}
-	},
-	{
-		"featureType":"landscape",	
-		"elementType":"geometry",
-		"stylers":{
-			"color":"#f5f5f5","lightness":20},
-	},
-	{
-		"featureType":"highway",
-		"elementType":"geometry.fill",
-		"stylers":
-			{"color":"#ffffff","lightness":17}
-	},
-	{
-		"featureType":"road.highway",
-		"elementType":"geometry.stroke",
-		"stylers":
-			{"color":"#ffffff","lightness":29,"weight":0.2}
-	},
-	{
-		"featureType":"arterial",
-		"elementType":"geometry",
-		"stylers":{
-			"color":"#ffffff","lightness":18}
-	},
-	{
-		"featureType":"local",
-		"elementType":"geometry",
-		"stylers":{
-			"color":"#ffffff","lightness":16}
-	},
-	{
-		"featureType":"poi",
-		"elementType":"geometry",
-		"stylers":{
-			"color":"#f5f5f5","lightness":21}
-	},
-	{
-		"featureType":"park",
-		"elementType":"geometry",
-		"stylers":{
-			"color":"#dedede","lightness":21}
-	},
-	{
-		"featureType":"all",
-		"elementType":"labels",
-		"stylers":{
-			"visibility":"on","color":"#ffffff","lightness":16}
-	}]
+var test123 =[{"featureType":"administrative","elementType":"all","stylers":{"saturation":"-100"}},{"featureType":"administrative.province","elementType":"all","stylers":{"visibility":"off"}},{"featureType":"landscape","elementType":"all","stylers":{"saturation":-100},{"lightness":65},{"visibility":"on"}},{"featureType":"poi","elementType":"all","stylers":{"saturation":-100},{"lightness":"50"},{"visibility":"simplified"}},{"featureType":"road","elementType":"all","stylers":{"saturation":"-100"}},{"featureType":"highway","elementType":"all","stylers":{"visibility":"simplified"}},{"featureType":"arterial","elementType":"all","stylers":{"lightness":"30"}},{"featureType":"local","elementType":"all","stylers":{"lightness":"40"}},{"featureType":"transit","elementType":"all","stylers":{"saturation":-100},{"visibility":"simplified"}},{"featureType":"water","elementType":"geometry","stylers":{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}},{"featureType":"water","elementType":"labels","stylers":{"lightness":-25},{"saturation":-100}}]
 
 
 
