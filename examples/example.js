2e73913ccd35c55e1a8c41f9c83e5f0319c892b3
chris2go <weiyq_chris@126.com>
2018-04-07 21:04:38 +0800

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -676,7 +676,17 @@ var mydefault11 = [{
                     }
           }]
 
-var test123 =[{"featureType":"administrative","elementType":"all","stylers":{"saturation":"-100"}},{"featureType":"administrative.province","elementType":"all","stylers":{"visibility":"off"}},{"featureType":"landscape","elementType":"all","stylers":{"saturation":-100},{"lightness":65},{"visibility":"on"}},{"featureType":"poi","elementType":"all","stylers":{"saturation":-100},{"lightness":"50"},{"visibility":"simplified"}},{"featureType":"road","elementType":"all","stylers":{"saturation":"-100"}},{"featureType":"highway","elementType":"all","stylers":{"visibility":"simplified"}},{"featureType":"arterial","elementType":"all","stylers":{"lightness":"30"}},{"featureType":"local","elementType":"all","stylers":{"lightness":"40"}},{"featureType":"transit","elementType":"all","stylers":{"saturation":-100},{"visibility":"simplified"}},{"featureType":"water","elementType":"geometry","stylers":{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}},{"featureType":"water","elementType":"labels","stylers":{"lightness":-25},{"saturation":-100}}]
+var test123 =[{"featureType":"administrative","elementType":"all","stylers":{"saturation":"-100"}},
+{"featureType":"administrative.province","elementType":"all","stylers":{"visibility":"off"}},
+{"featureType":"landscape","elementType":"all","stylers":{"saturation":-100,"lightness":65,"visibility":"on"}},
+{"featureType":"poi","elementType":"all","stylers":{"saturation":-100,"lightness":"50","visibility":"simplified"}},
+{"featureType":"road","elementType":"all","stylers":{"saturation":"-100"}},
+{"featureType":"highway","elementType":"all","stylers":{"visibility":"simplified"}},
+{"featureType":"arterial","elementType":"all","stylers":{"lightness":"30"}},
+{"featureType":"local","elementType":"all","stylers":{"lightness":"40"}},
+{"featureType":"transit","elementType":"all","stylers":{"saturation":-100,"visibility":"simplified"}},
+{"featureType":"water","elementType":"geometry","stylers":{"hue":"#ffff00","lightness":-25,"saturation":-97}},
+{"featureType":"water","elementType":"labels","stylers":{"lightness":-25,"saturation":-100}}]
 
 
 
